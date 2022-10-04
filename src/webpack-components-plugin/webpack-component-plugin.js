4698bc41b203505c22a5ceaa19abc132af63ffee
Miro Ruopsa <miroruopsa@gmail.com>
2022-10-04 23:04:06 +0300

    Files can now be included in build

--- src/webpack-components-plugin/webpack-component-plugin.js
+++ src/webpack-components-plugin/webpack-component-plugin.js
@@ -1,5 +1,17 @@
+const assert = require("assert");
 const fs = require("fs");
 const nodePath = require("path");
+
+const genericError = function genericError() {
+    const e = new Error();
+    const s = e.stack.split("\n").slice(2);
+    for (let i = 0; i < s.length; ++i) {
+        s[i] = s[i].trim();
+    }
+    console.log(`Error stack from ${s[0].split(" ")[1]}(...)`)
+    console.log(s);
+    throw `Something bad happened and brought you here.`;
+}
 
 const relativeToAbsolutePath = function relativeToAbsolutePath(path) {
     if (Array.isArray(path)) {
@@ -12,8 +24,8 @@ const relativeToAbsolutePath = function relativeToAbsolutePath(path) {
 }
 
 const findFilesWithExtRecursive = function findFilesWithExtRecursive(dirPath, ext = "") {
-    if (typeof(dirPath) !== "string" || typeof(ext) !== "string") {
-        throw("Invalid input types."); // Todo: fix this to be a bit better
+    if (typeof (dirPath) !== "string" || typeof (ext) !== "string") {
+        genericError();
     }
     const files = fs.readdirSync(dirPath);
     let arrayOfFiles = [];
@@ -38,21 +50,42 @@ const findFilesWithExtRecursive = function findFilesWithExtRecursive(dirPath, ex
 }
 
 class WebpackComponentsPlugin {
-    constructor() {
+    constructor(files) {
         this.root = "";
+        this.files = new Array(files.length);
+        for (let i = 0; i < this.files.length; ++i) {
+            this.files[i] = { in: relativeToAbsolutePath(files[i].in), out: files[i].out };
+        }
+    }
+
+    isIncludedInBuild = function isIncludedInBuild(path) {
+        for (let i = 0; i < this.files.length; ++i) {
+            if (this.files[i].in === path) {
+                return true;
+            }
+        }
+        return false;
     }
 
     apply(compiler) {
         const pluginName = WebpackComponentsPlugin.name;
         const { webpack } = compiler;
         const { RawSource } = webpack.sources;
-        console.log(RawSource)
+
         compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
             this.root = compilation.options.context;
-            const filesWithExt = findFilesWithExtRecursive(nodePath.join(this.root, "/src"), "js");
+            const filesWithExt = findFilesWithExtRecursive(nodePath.join(this.root, "/src"), "html");
             const filesAbs = relativeToAbsolutePath(filesWithExt);
-            
-            // Parse files
+
+            // Parse html files
+            for (let i = 0; i < filesAbs.length; ++i) {
+                if (!this.isIncludedInBuild(filesAbs[i])) {
+                    continue;
+                }
+                const file = fs.readFileSync(filesAbs[i], { encoding: "utf8" });
+                console.log(file);
+                // this.replaceHtmlComponents(file);
+            }
 
             // Combine component references
 
