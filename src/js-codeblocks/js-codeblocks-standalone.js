2a7d137a6618269afd1a754edc9e239261b0f1d1
Miro <38552014+hultsi@users.noreply.github.com>
2022-10-27 19:03:10 +0300

    readme updates

---
+++ src/js-codeblocks/js-codeblocks-standalone.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const nodePath = require("path");
+const {
+    relativeToAbsolutePath,
+    findFilesWithExtRecursive,
+} = require("./filesystem.js");
+const JsCodeBlocks = require("./js-codeblocks-core.js");
+
+// TODO
+
+module.exports = {};
